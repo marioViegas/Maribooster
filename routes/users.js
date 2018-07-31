596455888955d5edddb7ab835d770554cb20be21
Mário Viegas <mario.viegas.92@gmail.com>
2018-08-01 00:34:43 +0100

    begin angular

--- routes/users.js
+++ routes/users.js
@@ -5,25 +5,25 @@ const jwt = require('jsonwebtoken');
 const config = require('../config/database');
 const User = require('../models/user');
 
-//Register
+// Register
 router.post('/register', (req, res, next) => {
   let newUser = new User({
     name: req.body.name,
     email: req.body.email,
     username: req.body.username,
-    password: req.body.password,
+    password: req.body.password
   });
-  console.log(req);
+
   User.addUser(newUser, (err, user) => {
     if(err){
-      res.json({success: false, msg: 'Failed to register user'});
-    }else{
-      res.json({success: true, msg: 'User Registed'});
+      res.json({success: false, msg:'Failed to register user'});
+    } else {
+      res.json({success: true, msg:'User registered'});
     }
   });
 });
 
-//Authenticate
+// Authenticate
 router.post('/authenticate', (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
@@ -36,7 +36,7 @@ router.post('/authenticate', (req, res, next) => {
 
     User.comparePassword(password, user.password, (err, isMatch) => {
       if(err) throw err;
-      if(isMatch){
+      if(isMatch) {
         const token = jwt.sign({data: user}, config.secret, {
           expiresIn: 604800 // 1 week
         });
@@ -45,20 +45,20 @@ router.post('/authenticate', (req, res, next) => {
           success: true,
           token: `Bearer ${token}`,
           user: {
-              id: user._id,
-              name: user.name,
-              username: user.username,
-              email: user.email
-          }
+          id: user._id,
+          name: user.name,
+          username: user.username,
+          email: user.email
+        }
         });
       } else {
-        return res.json({success: false, msg: 'Wrong Password'});
+        return res.json({success: false, msg: 'Wrong password'});
       }
     });
   });
 });
 
-//Profile
+// Profile
 router.get('/profile', passport.authenticate('jwt', {session:false}), (req, res, next) => {
   res.json({user: req.user});
 });
